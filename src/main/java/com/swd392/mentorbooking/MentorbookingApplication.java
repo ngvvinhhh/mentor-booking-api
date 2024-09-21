5e31cd28116d27444d99dad39dffb9deb0dfa655
ngvvinhh <ngvvinh1813@gmail.com>
2024-09-21 07:42:09 +0700

    initialize project

---
+++ src/main/java/com/swd392/mentorbooking/MentorbookingApplication.java
@@ -0,0 +1,13 @@
+package com.swd392.mentorbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swd392/mentorbooking/MentorbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swd392.mentorbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
