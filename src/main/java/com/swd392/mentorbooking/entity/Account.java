e4b6a50766b6e43d2b426fc3570d6712ee7de067
ngvvinhh <ngvvinh1813@gmail.com>
2024-09-21 09:19:12 +0700

    initialize project

---
+++ src/main/java/com/swd392/mentorbooking/entity/Account.java
@@ -0,0 +1,16 @@
+package com.swd392.mentorbooking.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+
+}
